215ea75d625e875801db715b1ecf08a2cabb0576
jannisjr <joe_hannis@hotmail.com>
2023-05-09 17:01:19 +0100

    rspec for report length new test

--- spec/report_length_spec.rb
+++ spec/report_length_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "check report_length method" do
     result = report_length("happy")
     expect(result).to eq "This string was 5 characters long."
   end
+
+  it "returns the length of the string when given a number" do
+    result = report_length("happy birthday")
+    expect(result).to eq "This string was 14 characters long."
+  end
 end
